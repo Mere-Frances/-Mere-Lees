f2e0c726a1a5f69b0f4255ad6b0ec059691e811b
Mere Frances <meredithflss@gmail.com>
2024-08-23 21:12:16 +1200

    added images and descriptions to some individual projects

--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import projectData from '/public/projectData.json';
+import BackButton from '../components/BackButton';
+import prevArrow from '/public/images/prev.svg';
+import nextArrow from '/public/images/next.svg';
 
 const ProjectDetail = () => {
     const { id } = useParams();
+    const navigate = useNavigate();
     const project = projectData[id];
+
+    if (!project) {
+        return <h2>Project not found</h2>;
+    }
+
+    const handleNext = () => {
+        const nextId = (parseInt(id) + 1) % projectData.length;
+        navigate(`/project/${nextId}`);
+    };
+
+    const handlePrevious = () => {
+        const prevId = (parseInt(id) - 1 + projectData.length) % projectData.length;
+        navigate(`/project/${prevId}`);
+    };
 
     if (!project) {
         return <h2>Project not found</h2>;
@@ -12,29 +30,55 @@ const ProjectDetail = () => {
 
     return (
         <div className="project-detail--page">
+            <BackButton/>
             <div className="project-content-container">
                 <div className="project-left-border">
                     <h4>0{project.id}</h4>
                     <span className="vertical-span vertical-span-one"></span>
                     <span className="vertical-span vertical-span-two"></span>
                 </div>
-                <div className="project-content-grid">
-                    <h2 className="project-grid-item-1">{project.name} {project.description}</h2>
-                    <p className="project-grid-item-2">{project.description}</p>
-                    <div className="project-grid-item project-grid-item-3">
+                <div className="project-content-grid-1">
+                    {/* titles */}
+                    <div className="project-grid-item-1">
+                        <h2>{project.name} {project.description}</h2>
+                        <p>{project.biographyMajor}<br/></p>
+                        <p>{project.biographyMinor}</p>
+                        <div className="viewing-button">
+                            <div className="main-button">
+                                <a href={project.url}>See the real thing</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* med image */}
+                    <div className="project-grid-item project-grid-item-2">
                         <img src={project.imageMed}/>
                     </div>
-                    <div className="project-grid-item-4">
+                </div>
+                <div className="project-content-grid-2">
+                    {/* mockup */}
+                    <div className="project-grid-item-3">
                         <img src={project.mockup}/>
                     </div>
-                    <div className="project-grid-item project-grid-item-5">
+
+                    {/* large image */}
+                    <div className="project-grid-item project-grid-item-4">
                         <img src={project.imageLarge}/>
                     </div>
-                    <div className="project-grid-item-6"></div>
+                    <div className="project-grid-item-5"></div>
                 </div>
             </div>
             <div className="project-navigation-container">
-                <div className="project-overlap-img"></div>
+                <div className="page-nav-arrows">
+                    <div className="previous-arrow" onClick={handlePrevious}>
+                        <img src={prevArrow} alt="Previous Arrow" className="arrow-icon previous-arrow-icon" />                    </div>
+                    <div className="next-arrow" onClick={handleNext}>
+                        <img src={nextArrow} alt="Next Arrow" className="arrow-icon next-arrow-icon" />
+                    </div>
+                </div>
+                <div className="project-overlap-img">
+                    <img src={project.imageSmall}/>
+                </div>
             </div>
             {/* Add more project details here, and customize as needed */}
         </div>
